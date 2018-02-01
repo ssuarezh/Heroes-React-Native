246e1ba617616af23cdc076a077275520a976509
Santiago Suarez <ssuarezh@unbosque.edu.co>
2018-02-01 11:59:59 -0500

    Ejemplo simple de navigator stack

--- App.js
+++ App.js
@@ -4,25 +4,50 @@
  * @flow
  */
 
+//Ejemplo Navigator: https://reactnavigation.org/docs/intro/quick-start
 import React, { Component } from 'react';
+import { StackNavigator } from 'react-navigation';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 
 import LoginView from './src/components/loginView';
 
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-        <LoginView/>
-    );
-  }
-}
+const HomeScreen = ({ navigation }) => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
+    <Button
+      onPress={() => navigation.navigate('Details')}
+      title="Go to details"
+    />
+  </View>
+);
 
-const styles = StyleSheet.create({
+const DetailsScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Details Screen</Text>
+  </View>
+);
+
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      headerTitle: 'Home',
+    },
+  },
+  Details: {
+    screen: DetailsScreen,
+    navigationOptions: {
+      headerTitle: 'details',
+    },
+  },
 });
+
+export default RootNavigator;
